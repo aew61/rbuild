19d0909f77b18aca48dc1c009bd47bd721e676ef
Andrew Wood <aew61@case.edu>
2016-07-09 20:28:26 -0400

    build script for buildscripts

---
+++ LocalBuild.py
@@ -0,0 +1,32 @@
+# SYSTEM IMPORTS
+import os
+import platform
+import sys
+import tarfile
+
+
+currentDir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(currentDir, "scripts"))  # now we can import modules from <currentDirectory>/scripts
+# PYTHON PROJECT IMPORTS
+import Utilities
+
+
+if __name__ == "__main__":
+    # run flake8 on scripts/ directory
+    Utilities.PFork(appToExecute="flake8",
+                    argsForApp=["scripts/",
+                                "--config=%s" % (os.path.join(currentDir, "config", "flake8.cfg"))],
+                    failOnError=True)
+
+    buildString = "0.0.0.0"
+    tarFileName = "BuildScripts_%s.%s.%s.%s_src.tar.gz"
+    # bundle all directories and files into a tar.gz file and upload to share
+    with tarfile.open(tarFileName, "w:gz") as tarFile:
+        for item in os.listdir(currentDir):
+            if item != "LocalBuild.py" and "ReadMe" not in item:
+                tarFile.add(item)
+
+        # upload tarFile to shared directory
+        if buildString != "0.0.0.0":
+            share = os.environ("SHARE_PATH")
+            Utilities.copyTree(tarFileName, share)
