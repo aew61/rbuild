80c0148d567e179e2029d8e6784a072fb420a324
Andrew Wood <aew61@case.edu>
2016-07-25 15:59:02 -0400

    project name is the name of the database

--- scripts/ProjectBuild.py
+++ scripts/ProjectBuild.py
@@ -106,7 +106,7 @@ class ProjectBuild(MetaBuild):
                 insertOne=True)
             self._httpRequest.upload(packageDir,
                                      fileName=packageFileName + ".tar.gz",
-                                     urlParams=[os.environ["JOB_NAME"], self.config.lower()])
+                                     urlParams=[self._project_name, self.config.lower()])
 
     def help(self):
         print("command specific to project [%s]" % self._project_name)
