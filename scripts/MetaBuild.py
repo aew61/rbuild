57786b107e18726c6f15eb1e6d2fdef299d7aa86
Andrew Wood <aew61@case.edu>
2016-07-28 11:38:58 -0400

    forgot comma

--- scripts/MetaBuild.py
+++ scripts/MetaBuild.py
@@ -231,7 +231,7 @@ class MetaBuild(object):
             "-DCMAKE_INSTALL_PREFIX=%s" % allBuiltOutDir,  # install root dir
             "-DRUN_UNIT_TESTS=%s" % test,
             "-DENABLE_LOGGING=%s" % logging,
-            "-DCOVER=%s" % "TRUE" if self._cover else "FALSE"
+            "-DCOVER=%s" % "TRUE" if self._cover else "FALSE",
             "-DMONO_PATH=\"%s\"" % monoPath,
             "-DPYTHON_PATH=\"%s\"" % pythonPath,
             "-DPYTHON_VERSION=%s" % pythonVer,
